c2a445d4d44b3f090950af3643957dc14ec9940a
Cahyo Adi Prasetia <cahyoadiprasetia@gmail.com>
2024-02-28 00:39:31 +0700

    Modifikasi Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function() {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function() {
+    return 'NIM : 2241720191' . '<br>' . 'Nama : Cahyo Adi Prasetia';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/profile/{name}', function ($name) {
+    return "Nama Saya $name";
+})->name('profile');
+
+Route::get('/trueProfile', function() {
+    return to_route('profile', ['name' => 'Cahyo']);
+});
+   
