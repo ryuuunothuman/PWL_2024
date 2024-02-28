6e06191cf737aa0471879c715c3af4dc6a2a48d6
Cahyo Adi Prasetia <cahyoadiprasetia@gmail.com>
2024-02-28 09:32:19 +0700

    Meneruskan data ke View

--- routes/web.php
+++ routes/web.php
@@ -68,4 +68,5 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
-   
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
